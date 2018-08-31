2fc5aad8d17b01168ef22bb5273beaf5a25676b1
Justin Herrick <justin@lunarcollective.co>
2018-08-31 14:57:52 -0500

    Updating example files.

--- lib/install/examples/elm/Main.elm
+++ lib/install/examples/elm/Main.elm
@@ -1,54 +1,73 @@
-module Main exposing (..)
+module Main exposing (main)
 
+import Browser
 import Html exposing (Html, h1, text)
 import Html.Attributes exposing (style)
 
+
 -- MODEL
 
+
 type alias Model =
-  {
-  }
+    {}
+
+
 
 -- INIT
 
-init : (Model, Cmd Message)
+
+init : ( Model, Cmd Message )
 init =
-  (Model, Cmd.none)
+    ( Model, Cmd.none )
+
+
 
 -- VIEW
 
+
 view : Model -> Html Message
 view model =
-  -- The inline style is being used for example purposes in order to keep this example simple and
-  -- avoid loading additional resources. Use a proper stylesheet when building your own app.
-  h1 [style [("display", "flex"), ("justify-content", "center")]]
-     [text "Hello Elm!"]
+    -- The inline style is being used for example purposes in order to keep this example simple and
+    -- avoid loading additional resources. Use a proper stylesheet when building your own app.
+    h1 [ style [ ( "display", "flex" ), ( "justify-content", "center" ) ] ]
+        [ text "Hello Elm!" ]
+
+
 
 -- MESSAGE
 
+
 type Message
-  = None
+    = NoOp
+
+
 
 -- UPDATE
 
-update : Message -> Model -> (Model, Cmd Message)
+
+update : Message -> Model -> ( Model, Cmd Message )
 update message model =
-  (model, Cmd.none)
+    ( model, Cmd.none )
+
+
 
 -- SUBSCRIPTIONS
 
+
 subscriptions : Model -> Sub Message
 subscriptions model =
-  Sub.none
+    Sub.none
+
+
 
 -- MAIN
 
+
 main : Program Never Model Message
 main =
-  Html.program
-    {
-      init = init,
-      view = view,
-      update = update,
-      subscriptions = subscriptions
-    }
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
