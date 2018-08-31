01f9fc5aefc6e545d078e24b45181fe23139bf86
Justin Herrick <justin@lunarcollective.co>
2018-08-31 14:00:26 -0500

    Changing to 0.19 file name.

--- lib/install/elm.rb
+++ lib/install/elm.rb
@@ -29,7 +29,7 @@ say "Updating webpack paths to include .elm file extension"
 insert_into_file Webpacker.config.config_path, "- .elm\n".indent(4), after: /extensions:\n/
 
 say "Updating Elm source location"
-gsub_file "elm-package.json", /\"\.\"\n/,
+gsub_file "elm.json", /\"\.\"\n/,
   %("#{Webpacker.config.source_path.relative_path_from(Rails.root)}"\n)
 
 say "Updating .gitignore to include elm-stuff folder"
