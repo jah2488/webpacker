627b810245f245965e7166c38b3aa3c3b7f5d721
Justin Herrick <justin@lunarcollective.co>
2018-08-31 15:21:27 -0500

    Upgrading to elm 0.19.

--- lib/install/elm.rb
+++ lib/install/elm.rb
@@ -23,13 +23,13 @@ copy_file "#{__dir__}/examples/elm/Main.elm",
 say "Installing all Elm dependencies"
 run "yarn add elm elm-webpack-loader"
 run "yarn add --dev elm-hot-loader"
-run "yarn run elm package install -- --yes"
+run "yes Y | yarn run elm init"
 
 say "Updating webpack paths to include .elm file extension"
 insert_into_file Webpacker.config.config_path, "    - .elm\n", after: /extensions:\n/
 
 say "Updating Elm source location"
-gsub_file "elm-package.json", /\"\.\"\n/,
+gsub_file "elm.json", /\"\.\"\n/,
   %("#{Webpacker.config.source_path.relative_path_from(Rails.root)}"\n)
 
 say "Updating .gitignore to include elm-stuff folder"
